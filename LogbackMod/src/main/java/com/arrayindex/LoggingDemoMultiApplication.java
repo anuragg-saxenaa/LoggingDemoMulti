fd7dba4660e9caee41257cf1408a673976eaf0a1
Anurag Saxena <anuragg.saxenaa@gmail.com>
2022-01-27 21:20:04 -0500

    test log propagation from loback to log4j on maven multi module projects

---
+++ LogbackMod/src/main/java/com/arrayindex/LoggingDemoMultiApplication.java
@@ -0,0 +1,13 @@
+package com.arrayindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LoggingDemoMultiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingDemoMultiApplication.class, args);
+    }
+}
